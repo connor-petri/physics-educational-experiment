01188c0c42b1df8cc641f53a575aeb7c82fa5298
Connor <conn.petri@gmail.com>
2025-01-31 23:43:24 -0800

    Added vector2Test header and source files

---
+++ vectors/tests/Vector2Test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by connor on 1/31/25.
+//
+
+#include "Vector2Test.h"
+
+namespace peeTest {
+} // peeTest
---
+++ vectors/tests/Vector2Test.h
@@ -0,0 +1,16 @@
+//
+// Created by connor on 1/31/25.
+//
+
+#ifndef VECTOR2TEST_H
+#define VECTOR2TEST_H
+
+namespace peeTest {
+
+class Vector2Test {
+
+};
+
+} // peeTest
+
+#endif //VECTOR2TEST_H
