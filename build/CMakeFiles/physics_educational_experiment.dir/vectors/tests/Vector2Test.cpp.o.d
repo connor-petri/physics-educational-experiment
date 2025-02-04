8e7d4abd23d5612ee51c335095402e669506edac
Connor <conn.petri@gmail.com>
2025-02-04 00:51:24 -0800

    removed build folder

--- build/CMakeFiles/physics_educational_experiment.dir/vectors/tests/Vector2Test.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/physics_educational_experiment.dir/vectors/tests/Vector2Test.cpp.o: \
- /home/connor/Projects/physics-educational-experiment/vectors/tests/Vector2Test.cpp \
- /usr/include/stdc-predef.h \
- /home/connor/Projects/physics-educational-experiment/vectors/tests/Vector2Test.h
--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o: \
- /home/connor/Projects/physics-educational-experiment/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
- /usr/include/stdc-predef.h \
- /home/connor/Projects/physics-educational-experiment/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- /home/connor/Projects/physics-educational-experiment/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/connor/Projects/physics-educational-experiment/build/_deps/sfml-src/include/SFML/Config.hpp
