8ae466b98bf4d37818c49a27c19fa3d0e49e7449
Connor <conn.petri@gmail.com>
2025-02-04 16:59:09 -0800

    Basic Vector2Test class implimented

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 #include "PeeTest/PeeTest.h"
 #include "vectors/tests/Vector3Test.h"
+#include "vectors/tests/Vector2Test.h"
 #include <vector>
+
+#include "vectors/tests/Vector2Test.h"
 
 class AddTest : public peeTest::TestCase {
 public:
@@ -25,10 +28,11 @@ private:
 
 int main()
 {
-    using peeTest::Vector3Test;
+    using peeTest::Vector3Test, peeTest::Vector2Test;
     Vector3Test t0(1,1,1,1,1,1);
     Vector3Test t1(1,0,0,2,0,0); // Parallel
     Vector3Test t2(1,0,0,0,1,0); // Orthogonal
+    Vector2Test t3(1,1,1,1);
 
     peeTest::PeeTest::runAll();
 }
--- vectors/tests/Vector2Test.cpp
+++ vectors/tests/Vector2Test.cpp
@@ -5,4 +5,30 @@
 #include "Vector2Test.h"
 
 namespace peeTest {
+
+    Vector2Test::Vector2Test(float x1, float y1, float x2, float y2) {
+        this->v1 = new Vector2(x1, y1);
+        this->v2 = new Vector2(x2, y2);
+    }
+
+    Vector2Test::~Vector2Test() {
+        delete this->v1;
+        delete this->v2;
+    }
+
+    Result Vector2Test::run() {
+        if (this->v1->z() != 0.0f || this->v2->z() != 0.0f) {
+            this->result->append("Vector2Test::run() -> Vector2.z() should always return 0");
+            this->result->setFail();
+        }
+
+        Vector3 v = *this->v1 % *this->v2;
+        if (v.x() != 0.0f || v.y() != 0.0f) {
+            this->result->append("Vector2Test::run() -> The cross product between 2 Vector2 should always be 0.");
+            this->result->setFail();
+        }
+
+        return *this->result;
+    }
+
 } // peeTest
